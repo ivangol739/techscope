52c5bcb86dabc1b3ac8f2bba86bcf6de7ddbdaef
ivangol739 <ivangol739@gmail.com>
2025-02-12 22:39:17 +0300

    добавлено древовидное представление категорий

--- TechScope_cbv/settings.py
+++ TechScope_cbv/settings.py
@@ -35,12 +35,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.news.apps.NewsConfig',
+    'mptt',
+    'django_mptt_admin',
 ]
 
 MIDDLEWARE = [
