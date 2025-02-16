f8563e32a4fc7efe44ca44784243582aa35f3391
ivangol739 <ivangol739@gmail.com>
2025-02-16 23:03:36 +0300

    профили пользователей, представления и формы

--- TechScope_cbv/urls.py
+++ TechScope_cbv/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.news.urls')),
+    path('', include('apps.accounts.urls')),
 ]
 
 #Для работы media
