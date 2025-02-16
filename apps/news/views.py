f20f493ac01a17d1868d3900fcf3d312e14a0583
ivangol739 <ivangol739@gmail.com>
2025-02-16 23:27:10 +0300

    формы добавления и обновлениястатей

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,5 +1,9 @@
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category
+from .forms import PostCreateForm, PostUpdateForm
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import UpdateView
 
 
 class PostListView(ListView):
@@ -43,3 +47,49 @@ class PostFromCategoryView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Записи из категории: {self.category.title}'
         return context
+
+
+class PostCreateView(CreateView):
+    """
+    Представление: создание материалов на сайте
+    """
+    model = Post
+    template_name = 'news/post_create.html'
+    form_class = PostCreateForm
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи на сайт'
+        return context
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    """
+    Представление: обновления материала на сайте
+    """
+    model = Post
+    template_name = 'news/post_update.html'
+    context_object_name = 'post'
+    form_class = PostUpdateForm
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Обновление статьи: {self.object.title}'
+        return context
+
+    def form_valid(self, form):
+        # form.instance.updater = self.request.user
+        form.save()
+        return super().form_valid(form)
